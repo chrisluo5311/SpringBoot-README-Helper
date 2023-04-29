3ade6623e7768fced6c8aec3d8318da111dd508d
Chris Luo <chrislo5311@gmail.com>
2023-04-29 17:17:25 +0800

    add env

---
+++ src/main/java/com/helper/springbootreadmehelper/SpringBootReadmeHelperApplication.java
@@ -0,0 +1,16 @@
+package com.helper.springbootreadmehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chris
+ */
+@SpringBootApplication
+public class SpringBootReadmeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReadmeHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helper/springbootreadmehelper/SpringBootReadmeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helper.springbootreadmehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReadmeHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
